1f4b52ecb4325f4ff38cf2f07cbe026f8d262b14
Rakesh <111850784+ponkamrakesh@users.noreply.github.com>
2026-03-27 03:57:08 +0530

    Delete frontend directory

--- frontend/app.js
+++
@@ -1,6 +0,0 @@
-async function send() {
-  const input = document.getElementById("input").value;
-  const res = await fetch("/chat?q=" + input, { method: "POST" });
-  const data = await res.json();
-  document.getElementById("chat").innerHTML += input + " -> " + data.response + "\n";
-}
