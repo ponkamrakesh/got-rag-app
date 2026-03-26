c3a54ef076a486d44b93ebc0b615d3d819fb1431
Rakesh <111850784+ponkamrakesh@users.noreply.github.com>
2026-03-27 04:04:36 +0530

    Add PDF ingestion script

---
+++ backend/ingest.py
@@ -0,0 +1,33 @@
+from langchain.document_loaders import PyPDFLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from sentence_transformers import SentenceTransformer
+import pinecone
+from config import *
+
+def ingest_pdf(pdf_path="got.pdf"):
+    loader = PyPDFLoader(pdf_path)
+    docs = loader.load()
+
+    splitter = RecursiveCharacterTextSplitter(
+        chunk_size=500,
+        chunk_overlap=100
+    )
+
+    chunks = splitter.split_documents(docs)
+
+    model = SentenceTransformer(EMBED_MODEL)
+
+    texts = [chunk.page_content for chunk in chunks]
+    embeddings = model.encode(texts)
+
+    pinecone.init(api_key=PINECONE_API_KEY)
+    index = pinecone.Index(INDEX_NAME)
+
+    to_upsert = [
+        (str(i), embeddings[i].tolist(), {"text": texts[i]})
+        for i in range(len(texts))
+    ]
+
+    index.upsert(vectors=to_upsert)
+
+    print("PDF ingested into Pinecone")
